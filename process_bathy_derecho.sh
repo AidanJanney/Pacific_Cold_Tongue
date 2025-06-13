512475daf09b5ca47be5523658b43f8e8d5e23ce
Aidan Janney <aidan.janney@colorado.edu>
2025-06-13 00:38:32 -0600

    first commit

---
+++ process_bathy_derecho.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#PBS -N process_bathy.sh
+#PBS -A p93300012
+#PBS -l select=1:ncpus=32
+#PBS -l walltime=00:30:00
+#PBS -q main
+#PBS -m abe
+
+module purge
+module load ncarenv-basic/24.12  
+module load gcc/12.4.0
+module load cray-mpich/8.1.29
+module load esmf/8.8.0
+
+echo "Starting job at `date`"
+mpirun -np 30 ESMF_Regrid -s bathymetry_original_attrs.nc -d bathymetry_unfinished.nc -m bilinear --src_var depth --dst_var depth --netcdf4 --src_regional --dst_regional
